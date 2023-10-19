175970a284d1582f7d5c92321a224db7cc44ef61
AFranTill <TillFr@whs.school.nz>
2023-10-19 15:43:09 +1300

    hmm

--- Main.java
+++ Main.java
@@ -5,14 +5,12 @@
  * @version Verision 2 30/6/23
  */
 
-import java.util.Scanner;
-
 
 public class Main
 {
-    public static void Main(String[] args){
-        new TrainNetwork();
+    public static void Main(String[] args) {
+        //new TrainNetwork();
         //new GUI();
+        System.out.println("null");
     }
-    }
-
+}
